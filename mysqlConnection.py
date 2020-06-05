b0676e5fde7e2383d184735621b2f7caa8a82a2c
Juan Perez <juanfperez@me.com>
2020-06-05 09:28:34 -0400

    able to post into sql database

--- mysqlConnection.py
+++ mysqlConnection.py
@@ -49,41 +49,41 @@ for x in myresult:
 
         out_file.write('var mostPopularTeam = %s;' % json.dumps(myresult, indent = 3))
 
-# 
-# access_token = "1267470711209238531-4nsBlByecoMODDbuFPne7QUhtnc2rW"
-# access_token_secret = "rVZPSJXVam7MY1ZkAUFi4q0InSnbG7o3CYTEnveQqZ9hE"
-# consumer_key = "YDuuIEWVHtfGQQYJqqOH8CO1y"
-# consumer_secret = "DOXC7jWaMaMoTEt6pOfHN3qlsCHyTCnNptgVV9oA4R2uVlKlDq"
-#
-# auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-# auth.set_access_token(access_token, access_token_secret)
-# api = tweepy.API(auth)
-#
-# search_word = ("#ML2020")
-#
-# tweets = tweepy.Cursor(api.search,
-#                         q=search_word,
-#                         lang="en").items(1)
-#
-# sourcedTweets = list()
-#
-# for tweet in tweets:
-#     tj = tweet._json
-#     print(json.dumps(tj, indent = 3))
-#
-#
-#     sourcedTweets.append((tj['user']['name'], tweet.text))
-#
-#     print('=' *140)
-#     print('=' *140)
-#
-#     print(json.dumps(sourcedTweets, indent = 3))
-#
-# sql = "INSERT INTO marble (username, text) VALUES (%s, %s)"
-#
-# mycursor.execute(sql, sourcedTweets)
-#
-# mydb.commit()
+# pushing to mysql db
+access_token = "1267470711209238531-4nsBlByecoMODDbuFPne7QUhtnc2rW"
+access_token_secret = "rVZPSJXVam7MY1ZkAUFi4q0InSnbG7o3CYTEnveQqZ9hE"
+consumer_key = "YDuuIEWVHtfGQQYJqqOH8CO1y"
+consumer_secret = "DOXC7jWaMaMoTEt6pOfHN3qlsCHyTCnNptgVV9oA4R2uVlKlDq"
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+search_word = ("#ML2020")
+
+tweets = tweepy.Cursor(api.search,
+                        q=search_word,
+                        lang="en").items(3)
+
+sourcedTweets = list()
+
+for tweet in tweets:
+    tj = tweet._json
+    # print(json.dumps(tj, indent = 3))
+
+
+    sourcedTweets.append((tweet.created_at, tj['user']['name'], tweet.text))
+    # 
+    # print('=' *100)
+    # print('=' *100)
+    #
+    # # print(json.dumps(sourcedTweets, indent = 3))
+
+sql = "INSERT INTO tweets (post_date, user_name, comment) VALUES (%s, %s, %s)"
+
+mycursor.executemany(sql, sourcedTweets)
+
+mydb.commit()
 
 
 
